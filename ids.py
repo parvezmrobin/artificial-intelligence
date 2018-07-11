ad886328beb1d6972a03ea4067ac6c1414ed5cf5
Parvez M Robin <parvezmrobin@gmail.com>
2018-07-11 12:59:19 +0600

    ids depth limit

--- ids.py
+++ ids.py
@@ -32,23 +32,38 @@ for frm, to in edges:
     add_edge(frm, to)
 
 
-def dfs(graph, root, value):
+def match_value(val1, val2):
+    return val1 is val2
+
+
+def dfs(graph, root, value, depth_limit):
+    path = []
     q = LifoQueue()
     q.put(root)
     level = [-1] * len(graph)
+    level[root] = 0
     print("Searching sequence:", end=' ')
     while not q.empty():
         frm = q.get()
         print(frm, end=' ')
-        if frm is value:
-            return true
 
-        if not level[frm]:
-            level[frm] = true
-            for to in graph[frm]:
+        if depth_limit == level[frm]:
+            if match_value(frm, value):
+                return true
+            continue
+
+        for to in graph[frm]:
+            if level[to] is -1:
+                level[to] = level[frm] + 1
                 q.put(to)
 
     return false
 
 
-dfs(graph, 1, 7)
+max_depth = 20
+for i in range(4, max_depth):
+    if dfs(graph, 4, 5, i):
+        print('')
+        print("Found in depth {}".format(i))
+        break
+    print('')
